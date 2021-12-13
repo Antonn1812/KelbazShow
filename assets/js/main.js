992932292428f3050533473a331ee33b78e1292e
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-13 20:08:59 +0100

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,36 @@ let bg = setInterval(() => {
   )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }, 500);
 
-let playBtn = document.querySelector(".play-btn");
-let closeBtn = document.querySelector(".close-btn");
-
-closeBtn.addEventListener("click", () => {
-  gameOver();
-});
+// Function definition
 
 let gameOver = function () {
   clearInterval(bg);
   document.body.style.backgroundColor = "red";
   document.body.innerHTML = "Crashed ! Reload the page to retry.";
 };
+
+let startGame = function () {
+  document.querySelector(".centered").style.display = "none";
+  document.querySelector(".game-panel").style.display = "flex";
+
+}
+
+//<
+
+// Variable definition
+
+let qN = 1;
+
+let playBtn = document.querySelector(".play-btn");
+let closeBtn = document.querySelector(".close-btn");
+let logo = document.querySelector(".logo");
+
+//<
+
+closeBtn.addEventListener("click", () => {
+  gameOver();
+});
+
+playBtn.addEventListener("click", () => {
+  startGame();
+});
