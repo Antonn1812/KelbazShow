5c284f88104f4fccc0e076d560d987b983431ce9
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-13 11:05:53 +0100

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
-setInterval(() => {
+let bg = setInterval(() => {
   document.body.style.backgroundColor = `rgb(${Math.floor(
     Math.random() * 256
   )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }, 500);
+
+let playBtn = document.querySelector(".play-btn");
+let closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", () => {
+  gameOver()
+});
+
+let gameOver = function () {
+  while (true) {
+    console.log("DED !");
+
+    clearInterval(bg);
+    document.body.style.backgroundColor = "red";
+    document.body.innerHTML = "Crashed ! Reload the page to retry.";
+  }
+}
